b869e7397fd6b12316b92b014786e9e492c65bd8
Vesa Rautiainen <veskurau@gmail.com>
2024-03-21 15:22:18 +0200

    Add preliminary methods to Calculator and UI classes

--- src/calculator.py
+++ src/calculator.py
@@ -1,22 +1,81 @@
 class Calculator:
-    """Class, responsible for the application logic.....
+    """Class, responsible for the application logic.
     
     Attributes:
         expression: String, holds the expression in infix notation, given by the user
-    
+        rpn: String, holds the expression in postfix notation or Reverse Polish notation
+        saved_variables: Dictionary, keeps track of the saved results. 
+                         Key is the variable name and value is the result.
+
     """
 
     def __init__(self):
         """Class constructor, which creates a new calculator."""
 
-        self._expression = ""
+        self.expression = ""
+        self.rpn = ""
+        self.result = ""
+        self.saved_variables = {}
 
     def start(self, expression):
         """Class constructor, which creates a new calculator. 
 
         Args:
             expression (str): Expression in infix notation.
+            RPN (str): Expression in postfix notation.
         """
 
-        self._expression = expression
+        self.expression = expression
+
+        self.validate_expression(self.expression)
+        self.infix_to_rpn(self.expression)
+        self.rpn_to_result(self.rpn)
+
+        return self.result
+
+
+    def get_saved_variables(self):
+        """Gets all the saved variables from the
+
+        Returns:
+            Dictionary: Includes all the saved variables
+        """
+
+        return self.saved_variables
+
+    def validate_expression(self, expression):
+        """Performs an initial validation of the infix expression, before it is
+            entered to the shunting yard algorithm.
+        
+        Args:
+            expression (str): Expression in infix notation.
+        """
+
+        pass
+        # TODO: Add the necessary checks
+
+    def infix_to_rpn(self, expression):
+        """Uses the shunting yard algorithm to transform an infix expression to
+            a postfix expression.
+
+        Args: 
+            expression (str): Expression in infix notation.
+
+        Returns:
+            String: Expression in postfix or Reverse Polish Notation
+        """
+        pass
+        # TODO: Shunting yard algorithm comes here
+
+    def rpn_to_result(self, rpn):
+        """Uses a stack to calculate the result of a postfix expression.
+
+        Args: 
+            rpn (str): Expression in postfix notation.
+
+        Returns:
+            String: Result of the expression
+        """
+        pass 
+        # TODO: Create the function
 
--- src/ui.py
+++ src/ui.py
@@ -11,3 +11,45 @@ class UI:
         """Class constructor, which creates a new interface."""
 
         self._calculator = Calculator()
+
+    def start(self): 
+        """Starts up the interface for the user."""
+
+        print("Welcome to the Scientific Calculator!")
+
+        while True:
+            print()
+            print("Actions:")
+            print()
+            print("1) Enter expression")
+            print("2) Show saved variables")
+            print("I) Show instructions")
+            print("X) Exit calculator")
+            print()
+            user_input = input("Enter action: ")
+
+            if user_input == "X":
+                print("Good bye!")
+                break
+            elif user_input == "I":
+                self._show_instructions()
+            elif user_input == "1":
+                self._enter_expression()
+            elif user_input == "2":
+                self._get_saved_variables
+            else:
+                print("Incorrect input, please try again!")
+
+
+    def _show_instructions(self):
+        print("The instructions.....")
+        # TODO: add instructions
+
+    def _enter_expression(self):
+        expression = input("Enter expression: ")
+        self._calculator.start(expression)
+
+    def _get_saved_variables(self):
+        print(self._calculator.get_saved_variables())
+
+
